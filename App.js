0709626b033e39d1c3433ce4a608e63bda6a91c9
driartbytyqi <driartbytyqii@gmail.com>
2026-09-23 19:11:32 +0200

    wadadw

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import ButtonScreen from './ButtonScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <ButtonScreen/>
     </View>
   );
 }
---
+++ ButtonScreen.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, Button, StyleSheet , TouchableOpacity } from "react-native";
+
+
+const ButtonScreen = () => {
+    let counter , counterT = 0;
+    return(
+       <View>
+        <Text>Button click</Text>
+        <Button
+        title="Click me"
+        color="blue"
+        onPress={()=> console.log("Button Clicked" , counter++)} 
+        />
+
+
+        <TouchableOpacity
+          onPress={()=> console.log("TouchableOpacity Clicked" , counterT++)} 
+          >
+            <Text>Click TouchableOpacity</Text>
+            </TouchableOpacity>
+       </View>
+    );
+}
+
+
+
+
+
+export default ButtonScreen;
